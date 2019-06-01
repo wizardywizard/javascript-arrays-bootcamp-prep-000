efd2dae9b03e7ce0d056b204484fce5bb3a1d7f3
David Little <littledavid175@gmail.com>
2019-06-01 19:36:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ return [array,...element]
   }
   function destructivelyAddElementToEndOfArray (array,element) {
     array.push(element)
-    return
+    return array
     
   }
   function accessElementInArray (index,array) {
