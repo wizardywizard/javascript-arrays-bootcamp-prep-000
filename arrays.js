7b2935b96a0cb3b204704f81c46d184bd609d034
David Little <littledavid175@gmail.com>
2019-06-01 19:38:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ return [array,...element]
     
   }
   function removeElementFromBeginningOfArray (array) {
-    array.slice(1)
+    array.slice(0)
     
   }
   function destructivelyRemoveElementFromEndOfArray (array) {
